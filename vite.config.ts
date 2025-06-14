d4ad172a30342da727c38a1e776c014bc9c67134
ngmsola <ngmsola@gmail.com>
2025-06-14 19:42:42 +0800

    增加code-inspector-plugin用于调试

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import legacy from '@vitejs/plugin-legacy';
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig(() => ({
@@ -41,6 +42,9 @@ export default defineConfig(() => ({
                 'es/array/find-last-index',
                 'es/object/group-by',
             ],
+        }),
+        codeInspectorPlugin({
+            bundler: 'vite',
         }),
     ],
 }));
